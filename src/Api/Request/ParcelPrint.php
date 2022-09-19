acedee30bfd84133ec024cb6d8fcfb4f1284a2cd
Ernestas Luža <ernestas.luza@invertus.eu>
2022-09-19 17:55:13 +0300

    fix decoding

--- src/Api/Request/ParcelPrint.php
+++ src/Api/Request/ParcelPrint.php
@@ -52,15 +52,8 @@ class ParcelPrint
             );
         }
 
-        if (!is_array($response)) {
-            $decodedResponse = json_decode($response);
-            if (!isset($decodedResponse->status)) {
-                $decodedResponse['pdf'] = $response;
-                $responseBody = $serializer->deserialize($decodedResponse['pdf'], ParcelPrintResponse::class);
-            } else {
-                $responseBody = $serializer->deserialize($response, ParcelPrintResponse::class);
-            }
-        }
+        $responseBody = $serializer->deserialize(is_object($response) ? $response : json_decode($response),
+            ParcelPrintResponse::class);
 
         if ($responseBody->getStatus() === null) {
             $responseBody->setStatus('ok');
